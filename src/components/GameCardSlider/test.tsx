4975dc0b5f883abb0a92e1df48e13439f507d80f
DiegoCapella <diegocapelladev@gmail.com>
2022-04-22 19:12:47 -0300

    Create GameCardSlider component

---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,24 @@
+import { screen } from '@testing-library/react'
+import 'match-media-mock'
+import items from './mock'
+
+import GameCardSlider from '.'
+import { renderWithTheme } from '../../../utils/tests/helpers'
+
+describe('<GameCardSlider />', () => {
+  it('should render with 4 active items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should render white arrows if color passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+})
