40f10101245f8a11efccddf64b0c897f25e48d8c
DiegoCapella <diegocapelladev@gmail.com>
2022-04-25 10:35:56 -0300

    Create mock to Highlight and update Logo

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'Read Dead it is back',
+  subtitle: 'Come see John is now adventure',
+  backgroundImage: '/img/red-dead-background.png',
+  floatImage: '/img/red-dead-person.png',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
+}
--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,12 @@
 import { Story, Meta } from '@storybook/react'
 import Highlight, { HighlightProps } from '.'
+import item from './mock'
 
 export default {
   title: 'Highlight',
   component: Highlight,
   args: {
-    title: 'Read Dead it is back',
-    subtitle: 'Come see John is now adventure',
-    backgroundImage: '/img/red-dead-background.png',
-    floatImage: '/img/red-dead-person.png',
-    buttonLabel: 'Buy now',
-    buttonLink: '/rdr2'
+    ...item
   }
 } as Meta
 
