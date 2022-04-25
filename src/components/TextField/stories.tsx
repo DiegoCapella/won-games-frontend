327886150422c18cacda182ee6203840d57d806a
DiegoCapella <diegocapelladev@gmail.com>
2022-04-25 16:02:23 -0300

    Create TextField component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,37 @@
+import { Story, Meta } from '@storybook/react'
+import { Email } from '@styled-icons/material-outlined'
+
+import TextField, { TextFieldProps } from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    name: 'email',
+    placeholder: 'john.cage@gmail.com',
+    initialValue: '',
+    icon: <Email />,
+    disabled: false
+  },
+  argTypes: {
+    onInput: { action: 'changed' },
+    icon: { type: '' }
+  }
+} as unknown as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}
---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,86 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Email } from '@styled-icons/material-outlined'
+
+import TextField from '.'
+import { renderWithTheme } from '../../../utils/tests/helpers'
+
+describe('<TextField />', () => {
+  it('Renders without Label', () => {
+    renderWithTheme(<TextField />)
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('Renders with placeholder', () => {
+    renderWithTheme(<TextField placeholder="hey you" />)
+
+    expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument()
+  })
+
+  it('Renders with Icon', () => {
+    renderWithTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('Renders with Icon on the right side', () => {
+    renderWithTheme(
+      <TextField icon={<Email data-testid="icon" />} iconPosition="right" />
+    )
+
+    expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 })
+  })
+
+  it('Changes its value when typing', async () => {
+    const onInputChange = jest.fn()
+    renderWithTheme(
+      <TextField onInput={onInputChange} label="TextField" name="TextField" />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
+    })
+    expect(onInputChange).toHaveBeenCalledWith(text)
+  })
+
+  it('Does not changes its value when disabled', async () => {
+    const onInputChange = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInput={onInputChange}
+        label="TextField"
+        name="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    expect(input).toBeDisabled()
+
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text)
+    })
+    expect(onInputChange).not.toHaveBeenCalled()
+  })
+
+  it('Renders with error', () => {
+    renderWithTheme(
+      <TextField
+        icon={<Email data-testid="icon" />}
+        label="TextField"
+        error="Error message"
+      />
+    )
+
+    expect(screen.getByText('Error message')).toBeInTheDocument()
+  })
+})
