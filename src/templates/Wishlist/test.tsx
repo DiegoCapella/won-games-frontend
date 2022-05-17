c648d509e581799799d4a95d41b3b04e5a8839ff
DiegoCapella <diegocapelladev@gmail.com>
2022-05-17 16:48:17 -0300

    Create Wishlist page

---
+++ src/templates/Wishlist/test.tsx
@@ -0,0 +1,39 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from '../../../utils/tests/helpers'
+import Wishlist from '.'
+
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+  games: gamesMock,
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+}
+
+jest.mock('templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>
+  }
+}))
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />
+  }
+}))
+
+describe('<Wishlist />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Wishlist {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /wishlist/i })
+    ).toBeInTheDocument()
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(6)
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
+  })
+})
