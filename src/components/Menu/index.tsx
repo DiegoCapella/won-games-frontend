7cd39b528e67e7edcec7e89f3c43761eca7b91c6
DiegoCapella <diegocapelladev@gmail.com>
2022-05-17 15:37:51 -0300

    Add link for Home

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -25,12 +25,18 @@ const Menu = ({ username }: MenuProps) => {
         </S.IconsWrapper>
       </MediaMatch>
       <S.LogoWrapper>
-        <Logo hideOnMobile />
+        <Link href="/" passHref>
+          <a>
+            <Logo hideOnMobile />
+          </a>
+        </Link>
       </S.LogoWrapper>
 
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
@@ -54,7 +60,9 @@ const Menu = ({ username }: MenuProps) => {
       <S.MenuFull area-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
           {!!username && (
             <>
