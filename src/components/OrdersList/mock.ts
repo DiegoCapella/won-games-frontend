5db20f4d2d7b045bee5f794e3fd11017fdba21f0
DiegoCapella <diegocapelladev@gmail.com>
2022-05-19 15:11:01 -0300

    Create a OrdersList component

---
+++ src/components/OrdersList/mock.ts
@@ -0,0 +1,40 @@
+export default [
+  {
+    paymentInfo: {
+      id: '1',
+      flag: 'visa',
+      img: '/img/visa.png',
+      number: '**** **** **** 4242',
+      purchaseDate: 'Purchase made on Apr 14, 2021'
+    },
+    games: [
+      {
+        id: '1',
+        title: 'Red Dead Redemption 2',
+        downloadLink:
+          'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+        img: 'https://source.unsplash.com/user/willianjusten/151x70',
+        price: '$10.00'
+      }
+    ]
+  },
+  {
+    paymentInfo: {
+      id: '2',
+      flag: 'visa',
+      img: '/img/mastercard.png',
+      number: '**** **** **** 4444',
+      purchaseDate: 'Purchase made on Apr 14, 2021'
+    },
+    games: [
+      {
+        id: '2',
+        title: 'Borderlands 3',
+        downloadLink:
+          'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+        img: 'https://source.unsplash.com/user/willianjusten/151x70',
+        price: '$10.00'
+      }
+    ]
+  }
+]
