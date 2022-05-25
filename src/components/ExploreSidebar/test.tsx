e366512df1caec8c34eba2c421ff4d7bd3b2c909
DiegoCapella <diegocapelladev@gmail.com>
2022-05-25 14:24:23 -0300

    Create ExploreSidebar component

---
+++ src/components/ExploreSidebar/test.tsx
@@ -0,0 +1,54 @@
+import { screen } from '@testing-library/react'
+
+import ExploreSidebar from '.'
+import { renderWithTheme } from '../../../utils/tests/helpers'
+import items from './mock'
+
+describe('<ExploreSidebar />', () => {
+  it('should render headings', () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
+
+    expect(screen.getByRole('heading', { name: /price/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /sort by/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /genre/i })).toBeInTheDocument()
+  })
+
+  it('should render inputs', () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
+
+    expect(
+      screen.getByRole('checkbox', { name: /under \$50/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('radio', { name: /low to high/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the filter button', () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
+
+    expect(screen.getByRole('button', { name: /filter/i })).toBeInTheDocument()
+  })
+
+  it('should check initial values that are passed', () => {
+    renderWithTheme(
+      <ExploreSidebar
+        onFilter={jest.fn}
+        items={items}
+        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+        // onFilter={jest.fn}
+        // initialValues={{ platforms: ['windows'], sort_by: 'low-to-high' }}
+      />
+    )
+
+    expect(screen.getByRole('checkbox', { name: /windows/i })).toBeChecked()
+
+    expect(screen.getByRole('radio', { name: /low to high/i })).toBeChecked()
+  })
+})
