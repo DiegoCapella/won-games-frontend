b1602ed961e43c81180b98aaa724c54e7258b51b
DiegoCapella <diegocapelladev@gmail.com>
2022-07-07 19:13:12 -0300

    feat: add cartIcon badge

---
+++ src/components/CartIcon/styles.ts
@@ -0,0 +1,35 @@
+import { darken } from 'polished'
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    width: 2.4rem;
+    height: 2.4rem;
+    color: ${theme.colors.white};
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    position: relative;
+    transition: color ${theme.transition.default};
+
+    &:hover {
+      color: ${darken(0.3, theme.colors.white)};
+    }
+  `}
+`
+export const Badge = styled.span`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${theme.colors.secondary};
+    color: ${theme.colors.white};
+    font-size: 1rem;
+    border-radius: 50%;
+    width: 1.6rem;
+    height: 1.6rem;
+    position: absolute;
+    top: -0.4rem;
+    right: -0.4rem;
+  `}
+`
